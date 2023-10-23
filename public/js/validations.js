06b704e28c73337af5cef71ef9e47215c94f76fe
codebygustavo <hbw3.gustavo@gmail.com>
2023-10-23 11:15:26 -0300

    Adding pattern in HTML

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,60 +1,16 @@
-// esta validado, mas o retorno do usuário esta incompleto!
-'use strict';
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
 
-function isEmailValid(email) {
-    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return pattern.test(email);
-}
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
 
-function isPasswordValid(password) {
-    const pattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/;
-    return pattern.test(password);
-}
-
-function isConfirmpasswordValid(password, confirmPassword) {
-    return confirmPassword === password;
-}
-
-function authregister() {
-
-    const username = document.forms["registerAuth"]["username"];
-    const email = document.forms["registerAuth"]["email"];
-    const password = document.forms["registerAuth"]["password"];
-    const confirmpassword = document.forms["registerAuth"]["confirmpassword"];
-
-    if (!(username && username.length >= 4)) {
-        return false;
-    }
-
-    if (!(email && isEmailValid(email))) {
-        return false;
-    }
-
-    if (!(isPasswordValid(password) && password.length >= 6)) {
-        return false;
-    }
-
-    if (!isConfirmpasswordValid(password, confirmpassword)) {
-        return false;
-    }
-
-    return true;
-}
-
-const forms = document.querySelectorAll('.needs-validation');
-
-Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-        if (!authregister()) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
-        if (!form.checkValidity()) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
-        form.classList.add('was-validated');
-    }, false);
-});
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
